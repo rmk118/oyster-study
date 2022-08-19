5c1ab48921eed67b2bdb010945c37f1ac0a998be
rmk118 <rubykrasnow@gmail.com>
2022-08-19 19:03:35 -0400

    added new folder

--- Storage.R
+++ Storage.R
@@ -1,7 +1,6 @@
 #Storage document
 #Updated 8/19/22
 
-
 #Salinity data <25 (too harsh of a cutoff)
 
 #Salinity Plot no salinity <25
@@ -30,12 +29,6 @@ salinity.delayedStart.noLowerSalRolling
 
 #Six different salinity plots
 grid.arrange(allSalinity, salinityNoLowSal, salinityNoLowerSal, salinity.delayedStart, salinity.delayedStart.noLowerSal, salinity.delayedStart.noLowSal, ncol=2)
-
-
-
-
-
-
 
 #### R markdown tables
 
@@ -208,3 +201,297 @@ shortOne<-SamplingOne[SamplingOne$Height < 24,]
 # diffDay<-InsideCommonDay$Temp-OutsideCommonDay$Temp
 # mean(diffDay)
 # summary(diffDay)
+
+######################################################################
+######################### ANOVAs.R  ########################################
+
+# library(nlme)
+# library(lme4)
+# library(car)
+# library(MASS)
+# library(dplyr)
+# library(ARTool)
+# library(WRS2)
+
+#Read and subset data
+SamplingOne<-read.csv("6_14_22.csv", na.strings=c(""," ","NA"))
+SamplingOne<-subset(SamplingOne, select = c(Location,Gear,Treatment,Cage,Bag,Oyster,Height,Length,Width,Cup.ratio,Shell.shape))
+
+#Convert variables to factors
+SamplingOne<-within(SamplingOne, {
+  Cage<-as.factor(Cage)
+  Bag<-as.factor(Bag)
+  Location<-as.factor(Location)
+  Gear<-as.factor(Gear)
+  Treatment<-as.factor(Treatment)
+})
+str(SamplingOne)
+table(SamplingOne$Location, SamplingOne$Gear)
+
+######################### No random effects  ########################################
+
+#Shell height ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Regular ANOVA
+RegHeightOne<-aov(Height~ Gear * Location, data = SamplingOne)
+summary(RegHeightOne)
+#ART
+artHeightOne<-art(Height ~ Gear * Location, data=SamplingOne)
+anova(artHeightOne)
+#WRS
+wrsHeightOne<-t2way(Height ~ Gear * Location, data=SamplingOne)
+wrsHeightOne
+
+#Cup ratio ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Regular ANOVA
+RegRatioOne<-aov(Cup.ratio ~ Gear * Location, data = SamplingOne)
+summary(RegRatioOne)
+#ART
+artRatioOne<-art(Cup.ratio ~ Gear * Location, data=SamplingOne)
+anova(artRatioOne)
+#WRS
+wrsRatioOne<-t2way(Cup.ratio ~ Gear * Location, data=SamplingOne)
+wrsRatioOne
+
+#Shape Index ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Regular ANOVA
+RegShapeOne<-aov(Shell.shape ~ Gear * Location, data = SamplingOne)
+summary(RegShapeOne)
+#ART
+artShapeOne<-art(Shell.shape ~ Gear * Location, data=SamplingOne)
+anova(artShapeOne)
+#WRS
+wrsShapeOne<-t2way(Shell.shape ~ Gear * Location, data=SamplingOne)
+wrsShapeOne
+
+#Shell length ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Regular ANOVA
+RegLenOne<-aov(Length ~ Gear * Location, data = SamplingOne)
+summary(RegLenOne)
+#ART
+artLenOne<-art(Length ~ Gear * Location, data=SamplingOne)
+anova(artLenOne)
+#WRS
+wrsLenOne<-t2way(Length ~ Gear * Location, data=SamplingOne)
+wrsLenOne
+
+#Shell width ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Regular ANOVA
+RegWidOne<-aov(Width ~ Gear * Location, data = SamplingOne)
+summary(RegWidOne)
+#ART
+artWidOne<-art(Width ~ Gear * Location, data=SamplingOne)
+anova(artWidOne)
+#WRS
+wrsWidOne<-t2way(Width ~ Gear * Location, data=SamplingOne)
+wrsWidOne
+
+######################### Replicate 1-3, BP = NULL  ########################################
+
+SamplingTwo <- SamplingOne[order(SamplingOne$Gear),]
+BP.replicate<- SamplingTwo[SamplingTwo$Gear == "BP","Bag"]
+FB.replicate<- SamplingTwo[SamplingTwo$Gear == "FB","Bag"]
+FC.replicate<- SamplingTwo[SamplingTwo$Gear == "FC","Cage"]
+replicateColumn<-c(BP.replicate, FB.replicate, FC.replicate)
+SamplingTwo$Replicate<-replicateColumn
+SamplingTwo<-droplevels(SamplingTwo)
+str(SamplingTwo)
+
+#Shell Height ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Regular LM
+RegHeightTwo <- lmer(Height ~ Gear + Location + Gear:Location + (1 | Replicate), data = SamplingTwo, REML = FALSE)
+summary(RegHeightTwo)
+Anova(RegHeightTwo)
+#ART
+artHeightTwo<-art(Height ~ Gear * Location+ (1|Replicate), data=SamplingTwo)
+anova(artHeightTwo)
+
+######################### Replicate 1-3, BP = BPi/BPo  ########################
+
+SamplingThree<-SamplingTwo
+BP.replicate2<- SamplingThree[SamplingThree$Gear == "BP","Treatment"]
+replicateColumn2<-c(BP.replicate2, FB.replicate, FC.replicate)
+SamplingThree$Replicate<-replicateColumn2
+str(SamplingThree)
+SamplingThree<-droplevels(SamplingThree)
+
+#Shell Height ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Regular LM
+RegHeightThree <- lmer(Height ~ Gear + Location + Gear:Location + (1 | Replicate), data = SamplingThree, REML = FALSE)
+summary(RegHeightThree)
+Anova(RegHeightThree)
+#ART
+artHeightThree<-art(Height ~ Gear * Location+ (1|Replicate), data=SamplingThree)
+anova(artHeightThree)
+
+#### Replicate 1-12, BP = BPi/BPo  #######################
+
+# IndividualOysters$Replicate<-as.factor(IndividualOysters$Replicate)
+# str(IndividualOysters)
+# Inside<-IndividualOysters[which(IndividualOysters$Location=="Inside"),]
+# InsideOne<-Inside[which(Inside$Cage==1),]
+# InsideOne$Replicate<-as.factor(7)
+#   
+#   
+# IndividualOysters$Replicate[IndividualOysters$Cage==1]<-as.factor(7)
+# IndividualOysters$Replicate[IndividualOysters$Cage==2]<-as.factor(8)
+# IndividualOysters$Replicate[IndividualOysters$Cage==3]<-as.factor(9)
+# IndividualOysters$Replicate[IndividualOysters$Cage==4]<-as.factor(10)
+# IndividualOysters$Replicate[IndividualOysters$Cage==5]<-as.factor(11)
+# IndividualOysters$Replicate[IndividualOysters$Cage==6]<-as.factor(12)
+
+#lm with no random effect; no significance
+alignedOystersHeight<-art(Height ~ Gear * Location, data=IndividualOysters)
+anova(alignedOystersHeight)
+
+#mixed effects linear model with replicate as random effect; no significance
+alignedOystersHeight2<-art(Height ~ Gear * Location + (1|Replicate), data=IndividualOysters)
+anova(alignedOystersHeight2)
+
+#mixed effects linear model with bag as random effect; location significant
+alignedOystersHeight3<-art(Height ~ Gear * Location + (1|Bag), data=IndividualOysters)
+anova(alignedOystersHeight3)
+
+####################################################################
+######################### ModelOptions.R  ########################################
+
+# library(nlme)
+# library(lme4)
+# library(car)
+# library(MASS)
+# library(dplyr)
+# library(ARTool)
+# #library(WRS2)
+
+#Read and subset data
+SamplingOne<-read.csv("6_14_22.csv", na.strings=c(""," ","NA"))
+SamplingOne<-subset(SamplingOne, select = c(Location,Gear,Treatment,Cage,Bag,Oyster,Height,Length,Width,Cup.ratio,Shell.shape))
+
+#Convert variables to factors
+SamplingOne<-within(SamplingOne, {
+  Cage<-as.factor(Cage)
+  Bag<-as.factor(Bag)
+  Location<-as.factor(Location)
+  Gear<-as.factor(Gear)
+  Treatment<-as.factor(Treatment)
+})
+str(SamplingOne)
+table(SamplingOne$Location, SamplingOne$Gear)
+
+#Regular ANOVA no random effects
+RegCupRatioOne<-aov(Cup.ratio~ Gear * Location, data = SamplingOne)
+summary(RegCupRatioOne)
+
+#ART one: no replicates included as random effect
+artCupRatioOne<-art(Cup.ratio ~ Gear * Location, data=SamplingOne)
+anova(artCupRatioOne)
+
+artShellShapeOne<-art(Shell.shape ~ Gear * Location, data=SamplingOne)
+anova(artShellShapeOne)
+
+FB<-SamplingOne[SamplingOne$Gear=="FB",]
+ggplot(data = FB, aes(x = Location, y = Cup.ratio, fill=Bag))+geom_boxplot()+scale_y_continuous(limits=c(0,0.5))+ylab("Cup ratio (shell width/height)")+theme_classic()
+
+FC<-SamplingOne[SamplingOne$Gear=="FC",]
+ggplot(data = FC, aes(x = Location, y = Cup.ratio, fill=Cage))+geom_boxplot()+scale_y_continuous(limits=c(0,0.5))+ylab("Cup ratio (shell width/height)")+theme_classic()
+
+FB2<-SamplingOne[SamplingOne$Gear=="FB",]
+ggplot(data = FB2, aes(x = Location, y = Shell.shape, fill=Bag))+geom_boxplot()+scale_y_continuous(limits=c(0,8))+ylab("Shell shape")+theme_classic()
+
+df2<-summarise(group_by(FB2, Bag),Mean=mean(Shell.shape),SD=sd(Shell.shape))
+
+#CAGE ONE IS MUCH HIGHER ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+FC2<-SamplingOne[SamplingOne$Gear=="FC",]
+ggplot(data = FC2, aes(x = Location, y = Shell.shape, fill=Cage))+geom_boxplot()+scale_y_continuous(limits=c(0,8))+ylab("Shell shape")+theme_classic()
+
+FC2outside<-FC2[FC2$Location=="Outside",]
+df3<-summarise(group_by(FC2, Cage),Mean=mean(Shell.shape),SD=sd(Shell.shape))
+df4<-summarise(group_by(FC2outside, Cage),Mean=mean(Shell.shape),SD=sd(Shell.shape))
+
+#WRS
+wrsCupRatioOne<-t2way(Cup.ratio ~ Gear * Location, data=SamplingOne)
+wrsCupRatioOne
+
+SamplingOne <- SamplingOne[order(SamplingOne$Gear),]
+BP.replicate<- SamplingOne[SamplingOne$Gear == "BP","Bag"]
+FB.replicate<- SamplingOne[SamplingOne$Gear == "FB","Bag"]
+FC.replicate<- SamplingOne[SamplingOne$Gear == "FC","Cage"]
+replicateColumn<-c(BP.replicate, FB.replicate, FC.replicate)
+SamplingOne$Replicate<-replicateColumn
+SamplingOne<-droplevels(SamplingOne)
+str(SamplingOne)
+
+#Regular LM (REML true or false?) -- modify this formula to try parametric LM with other random effect configurations
+RegCupRatioTwo <- lmer(Cup.ratio ~ Gear + Location + Gear:Location + (1 | Replicate), data = SamplingOne, REML = FALSE)
+summary(RegCupRatioTwo)
+Anova(RegCupRatioTwo)
+
+#ART two: replicates 1,2,3 for all floating treatments, BP replicate = null
+artCupRatioTwo<-art(Cup.ratio ~ Gear * Location+ (1|Replicate), data=SamplingOne)
+anova(artCupRatioTwo)
+
+BP.replicate2<- SamplingOne[SamplingOne$Gear == "BP","Treatment"]
+replicateColumn2<-c(BP.replicate2, FB.replicate, FC.replicate)
+SamplingOne$Replicate2<-replicateColumn2
+SamplingOne<-droplevels(SamplingOne)
+str(SamplingOne)
+
+#ART three: replicates 1,2,3 for all floating treatments, BP replicate = BPi or BPo
+artCupRatioThree<-art(Cup.ratio ~ Gear * Location+ (1|Replicate2), data=SamplingOne)
+anova(artCupRatioThree)
+
+SamplingTwo<-read.csv("6_14_22_2.csv", na.strings=c(""," ","NA"))
+SamplingTwo<-subset(SamplingTwo, select = c(Location,Gear,Treatment,Cage,Bag,Oyster,Height,Length,Width,Cup.ratio,Shell.shape))
+
+#Convert variables to factors
+SamplingTwo<-within(SamplingTwo, {
+  Cage<-as.factor(Cage)
+  Bag<-as.factor(Bag)
+  Location<-as.factor(Location)
+  Gear<-as.factor(Gear)
+  Treatment<-as.factor(Treatment)
+})
+
+SamplingTwo <- SamplingTwo[order(SamplingTwo$Gear),]
+BP.replicate3<- SamplingTwo[SamplingTwo$Gear == "BP","Bag"]
+FB.replicate2<- SamplingTwo[SamplingTwo$Gear == "FB","Bag"]
+FC.replicate2<- rep(c(7,8,9,10,11,12),each=32)
+replicateColumn3<-c(BP.replicate3, FB.replicate2, FC.replicate2)
+SamplingTwo$Replicate<-as.factor(replicateColumn3)
+SamplingTwo<-droplevels(SamplingTwo)
+str(SamplingTwo)
+
+#ART four: replicates 1-12 for floating gear, BP replicate = null
+artCupRatioFour<-art(Cup.ratio ~ Gear * Location+ (1|Replicate), data=SamplingTwo)
+anova(artCupRatioFour)
+
+BP.replicate4<- SamplingTwo[SamplingTwo$Gear == "BP","Treatment"]
+replicateColumn4<-c(rep(c("BPi","BPo"),each=96), FB.replicate2, FC.replicate2)
+SamplingTwo$Replicate2<-as.factor(replicateColumn4)
+SamplingTwo<-droplevels(SamplingTwo)
+str(SamplingTwo)
+
+#ART five: replicates 1-12 for floating gear (bag for FB, cage for FC), BP replicate = BPi/BPo
+artCupRatioFive<-art(Cup.ratio ~ Gear * Location+ (1|Replicate2), data=SamplingTwo)
+anova(artCupRatioFive)
+
+BP.replicate5<- SamplingTwo[SamplingTwo$Gear == "BP","Bag"]
+FB.replicate3<- SamplingTwo[SamplingTwo$Gear == "FB","Bag"]
+FC.replicate3<- SamplingTwo[SamplingTwo$Gear == "FC","Bag"]
+replicateColumn5<-c(BP.replicate5, FB.replicate3, FC.replicate3)
+SamplingTwo$Replicate3<-as.factor(replicateColumn5)
+SamplingTwo<-droplevels(SamplingTwo)
+str(SamplingTwo)
+
+#ART six: replicates 1-30 for floating gear (bags for both), BP replicate = null
+artCupRatioSix<-art(Cup.ratio ~ Gear * Location+ (1|Replicate3), data=SamplingTwo)
+anova(artCupRatioSix)
+
+BP.replicate6<- SamplingTwo[SamplingTwo$Gear == "BP","Treatment"]
+replicateColumn6<-c(BP.replicate6, FB.replicate3, FC.replicate3)
+SamplingTwo$Replicate4<-as.factor(replicateColumn6)
+SamplingTwo<-droplevels(SamplingTwo)
+str(SamplingTwo)
+
+#ART seven: replicates 1-30 for floating gear (bags for both), BP replicate = BPi/BPo
+artCupRatioSeven<-art(Cup.ratio ~ Gear * Location+ (1|Replicate4), data=SamplingTwo)
+anova(artCupRatioSeven)
